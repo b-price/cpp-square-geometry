7f0d62f6faca3ca6fd58ce29092cf6e243b93e4f
b-price <bprice12@students.towson.edu>
2024-02-12 12:31:01 -0500

    Initial Commit

---
+++ 439Assignment0/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  439Assignment0
+//
+//  Created by Benjamin Price on 2/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
