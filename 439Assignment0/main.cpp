9ecf724682c586874dcf0f3974c83a0fe2611c26
b-price <bprice12@students.towson.edu>
2024-02-12 12:53:28 -0500

    Initial commit of square geometry app

--- 439Assignment0/main.cpp
+++ 439Assignment0/main.cpp
@@ -4,11 +4,34 @@
 //
 //  Created by Benjamin Price on 2/12/24.
 //
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+double squareArea(double side){
+    return side * side;
+}
+
+double squarePerimeter(double side){
+    return side * 4;
+}
+
+// Driver code
+int main()
+{
+    // Declare the variables
+    double num;
+        
+    cout << "Square Geometry\n";
+
+    // Input the side length
+    cout << "Enter side length of square: ";
+    cin >> num;
+
+    // Shows area
+    cout << "Square's area is: " << squareArea(num) << "\n";
+        
+    // Shows perimeter
+    cout << "Square's perimeter is: " << squarePerimeter(num) << "\n";
+
     return 0;
 }
